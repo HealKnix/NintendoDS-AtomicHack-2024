845b82378566455d6f3c63ede06f24b6c8c06aab
Daniil Mikhaylov <www.daniiland@gmail.com>
2024-06-15 13:12:13 +0800

    добавил ключ для api'шки

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+import hashlib
 
 app = FastAPI()
 
 @app.get("/")
-def root():
+def root(key: str):
+  hash = hashlib.sha256()
+  hash.update(key.encode())
+  
+  if hash.hexdigest() == "74323a1db24ecc5a514efa9b8409e0f899249d76cbfba51ce252595818d8b2c3":
+    print('yes')
+  else:
+    print('no')
+  
   return {"Hello": "World"}
